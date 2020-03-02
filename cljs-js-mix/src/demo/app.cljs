097364c05255cd8ce06e0582f5c88e7066800063
Ky Ky <kyky@kyky.ky>
2020-03-02 12:05:00 +0300

    *

--- cljs-js-mix/src/demo/app.cljs
+++ cljs-js-mix/src/demo/app.cljs
@@ -1,6 +1,6 @@
 (ns demo.app
   (:require
-   ["/gen/bar" :as bar :refer (sum)]
+   ["/__gen__/bar" :as bar :refer (sum)]
    [clojure.test.check :as tc]
    [clojure.test.check.generators :as gen]
    [clojure.test.check.properties :as prop :include-macros true]))
