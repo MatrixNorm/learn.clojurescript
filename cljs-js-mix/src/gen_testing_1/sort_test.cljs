774d41a66ab9b7bb26f3fbf6effde7b64d42382b
Ky Ky <kyky@kyky.ky>
2020-03-02 13:57:54 +0300

    *

--- cljs-js-mix/src/gen_testing_1/sort_test.cljs
+++ cljs-js-mix/src/gen_testing_1/sort_test.cljs
@@ -1,10 +1,16 @@
 (ns gen-testing-1.sort-test
   (:require [clojure.test.check :as tc]
             [clojure.test.check.generators :as gen]
-            [clojure.test.check.properties :as prop :include-macros true]))
+            [clojure.test.check.properties :as prop :include-macros true]
+            [clojure.test.check.clojure-test :refer-macros [defspec]]
+            [cljs.test :refer (deftest is)]))
 
-(def sort-idempotent-prop
+(defspec first-element-is-min-after-sorting 
+  100
+  (prop/for-all [v (gen/not-empty (gen/vector gen/int))]
+                (= (apply min v)
+                   (first (sort v)))))
+
+(defspec sort-is-idempotent 100
   (prop/for-all [v (gen/vector gen/int)]
                 (= (sort v) (sort (sort v)))))
-
-(tc/quick-check 100 sort-idempotent-prop)
